4b8aa52d975b331854a14ae315fb2976fdbced93
Tim Ekl <lithium3141@gmail.com>
2011-05-23 04:14:58 -0400

    Implement Basic authentication
    Signed-off-by: Tim Ekl <lithium3141@gmail.com>

--- wos.rb
+++ wos.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require 'base64'
 require 'optparse'
 require 'pp'
 require 'socket'
@@ -42,6 +43,21 @@ def log_verbose(s)
   end
 end
 
+# Define authentication helper(s)
+def accept_auth(user, pass)
+  if @options[:user] == nil && @options[:pass] == nil
+    return true
+  elsif @options[:user] == user && @options[:pass] == nil
+    return true
+  elsif @options[:user] == nil && @options[:pass] == pass
+    return true
+  elsif @options[:user] == user && @options[:pass] == pass
+    return true
+  else
+    return false
+  end
+end
+
 # Require a file to serve
 if ARGV.length != 1 && !(ARGV.length == 0 && @options[:serve_self] == true)
   $stderr.puts "No file specified and --serve-self option not passed; aborting"
@@ -61,21 +77,41 @@ log_verbose("Read #{contents.length} bytes")
 serv = TCPServer.new(@options[:port])
 log_verbose("Listening on port #{@options[:port]}")
 while s = serv.accept
-  req = s.gets
-  log_verbose("#{s.peeraddr[2]}: #{req}")
+  # Read HTTP request
+  req = []
+  while l = s.gets
+    req.push l
+    if l == "\r\n"
+      break
+    end
+  end
+  log_verbose("#{s.peeraddr[2]}: #{req[0]}")
   served = false
+
+  # Fetch username/pass out of request
+  user = nil
+  pass = nil
+  req.each do |r|
+    if r[/^Authorization: Basic /] == "Authorization: Basic "
+      auth_b64 = /^Authorization: Basic (.*)$/.match(r)[1]
+      auth = Base64.decode64(auth_b64)
+      user = auth.split(":")[0]
+      pass = auth.split(":")[1]
+      log_verbose("Credentials: #{user} : #{pass}")
+    end
+  end
   
   # Parse file from request; redirect if necessary
-  req_file = req.split(" ")[1][1..-1]
-  if req_file == File.basename(file)
-    s.print "HTTP/1.1 200/OK\r\n\r\n" + contents
-    s.close
-    served = true
+  req_file = req[0].split(" ")[1][1..-1]
+  if accept_auth(user, pass)
+    if req_file == File.basename(file)
+      s.print "HTTP/1.1 200/OK\r\n\r\n" + contents
+      s.close
+      break
+    else
+      s.print "HTTP/1.1 301/Moved Permanently\r\nLocation: #{File.basename(file)}\r\n\r\n"
+    end
   else
-    s.print "HTTP/1.1 301/Moved Permanently\r\nLocation: #{File.basename(file)}\r\n\r\n"
-  end
-
-  if served
-    break
+    s.print "HTTP/1.1 401/Unauthorized\r\nWWW-Authenticate: basic\r\n\r\n"
   end
 end
